5ff5775f4814ec625c3b6a132f616230741729d0
Sanchit Sharma <sanchit.sharma99@gmail.com>
2019-05-06 07:24:15 +0530

    All done , UI working perfectly and nothing needs to be done !

---
+++ Assets/Scripts/Scene_loader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_loader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void loadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        
+    }
+}
